7ac8a689ec2171d327d135801537490957e68af9
ritzyRosabel <rosabelolugbenga@yahoo.com>
2020-07-01 01:20:45 +0100

    adding IValidatableObject Interface

--- IvalidatorObjectTest/LoginModel.cs
+++ IvalidatorObjectTest/LoginModel.cs
@@ -1,14 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace IvalidatorObjectTest
 {
-    public class LoginModel:Ivalidatable
+    public class LoginModel:IValidatableObject
     {
         public string Username { get; set; }
         public string Password { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
